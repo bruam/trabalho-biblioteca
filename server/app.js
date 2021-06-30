0c36b318fa90bf555716997f1753622b3dc7c9fa
Bruno Martins <brunoagmart@gmail.com>
2021-06-29 22:13:41 -0300

    Client deletado

--- server/app.js
+++ server/app.js
@@ -1,10 +1,9 @@
 const express = require("express")
+const cors = require("cors");
 const app = express()
 const PORT = 3000
-const cors = require("cors");
 
 app.use(cors());
-app.use(express.static(__dirname + '/client'));
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
